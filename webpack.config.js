6fdb317db8a7ec0e11775b436095987e772d8e1e
Alice Armstrong <22509772+AliceArmstrong@users.noreply.github.com>
2020-03-06 21:04:31 +0000

    Clean up comments

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,9 @@
-// const path = require("path");
-// const getFilesFromDir = require("./config/files");
-// const PAGE_DIR = path.join("src", "pages", path.sep);
-//
-// const HtmlWebPackPlugin = require("html-webpack-plugin");
-// const htmlFiles = getFilesFromDir(PAGE_DIR, [".html"]);
-//
-// const jsFiles = getFilesFromDir(PAGE_DIR, [".js"]);
-// const entry = jsFiles.reduce( (obj, filePath) => {
-//     const entryChunkName = filePath.replace(path.extname(filePath), "").replace(PAGE_DIR, "");
-//     obj[entryChunkName] = `./${filePath}`;
-//     return obj;
-// }, {});
-//
-// const htmlPlugins = htmlFiles.map( filePath => {
-//   const fileName = filePath.replace(PAGE_DIR, "");
-//   return new HtmlWebPackPlugin({
-//     chunks:[fileName.replace(path.extname(fileName), ""), "vendor"],
-//     template: filePath,
-//     filename: fileName})
-// });
-//
-// module.exports = {
-//     entry: entry,
-//     // mode: 'production',
-//     plugins:[...htmlPlugins],
-//     resolve: {
-//         alias: {
-//             src: path.resolve(__dirname, "src"),
-//             components: path.resolve(__dirname, "src", "components")
-//         }
-//     },
-//     module: {
-//         rules: [{
-//             test: /\.js$/,
-//             exclude: /node_modules/,
-//             use: {
-//                 loader: 'babel-loader',
-//                 options: {
-//                     presets: [
-//                       '@babel/preset-env',
-//                       '@babel/preset-react'
-//                     ]
-//                 }
-//             }
-//         }]
-//     },
-// };
-
 const path = require('path');
 const SRC_DIR = path.join(__dirname, '/src');
 const DIST_DIR = path.join(__dirname, '/dist');
 const webpack = require('webpack');
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
-  // entry: '/src/index.jsx',
   output: {
     path: DIST_DIR,
     filename: 'bundle.js',
